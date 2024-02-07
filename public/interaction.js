95a278b5b0abaee7af1bd93eca2fb7f396aa9f11
KaanKalmi <kaan.kalmioglu@hva.nl>
2024-02-07 11:39:15 +0100

    finished

--- public/interaction.js
+++ public/interaction.js
@@ -2,5 +2,19 @@ let card = document.querySelector('.card');
 
 card.addEventListener('click', function() {
     card.classList.toggle('container-flip');
+});
+
+let theme = new Audio('images/themesong.mp3');
+
+card.addEventListener('mouseenter', handleMouse);
+card.addEventListener('mouseleave', handleMouse);
+
+function handleMouse(event) {
+    if (event.type === 'mouseenter') {
+        theme.play();  
+        console.log('Mouse enter!');
+    } else if (event.type === 'mouseleave') {
+        theme.pause();
+        console.log('Mouse leave!');
     }
-);
+}
